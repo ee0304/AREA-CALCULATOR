8d55f5e28c2c989d752749e4d5e4329a0695950d
ee0304 <104321381+ee0304@users.noreply.github.com>
2022-04-25 00:08:50 +0530

    Update and rename README.md to BHARATH .C
    THIS CODE HELPS US TO FIND THE AREA OF A  RECTANAGLE

---
+++ BHARATH .C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+   int l,b,area,peri;
+      scanf("%d\n",&l);
+        scanf("%d\n",&b);
+           area=l*b;
+        peri=2*(l+b);
+if (area>peri) 
+{
+     printf("Area\n%d",area);
+}
+     else if (area<peri)
+{
+     printf("Peri\n%d",peri);
+}
+      else  
+{
+    printf("Eq\n%d",area); 
+}
+	return 0;
+}
